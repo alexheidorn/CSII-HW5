5cc7df00b28b4b9cd545c5bc8ca94509f81f5784
alexheidorn <alexheidorn12@gmail.com>
2023-10-03 16:35:12 -0500

    most recent changes

--- hw5.cpp
+++ hw5.cpp
@@ -1,6 +1,6 @@
 /*
 * Alex Heidorn
-* Teammates: Sam, Aidan, Wolf
+* Teammates: Sam Draeger, Aidan Carlson, Anna Wolf
 * Created: 10-3-23
 * CS2 HW5
 *  
@@ -12,6 +12,16 @@
 
 using namespace std;
 using namespace std::chrono;
+int bSrchHelper(int v, int A[], int low, int hgh) {
+  if (low > hgh) { return -1; }
+  int mid = (low + hgh) / 2;
+  if (A[mid] < v) { return bSrchHelper(v,A,mid+1,hgh); }
+  if (A[mid] > v) { return bSrchHelper(v,A,low,mid-1); }
+  return mid;
+}
+int bSrch(int v, int A[], int N) {
+  return bSrchHelper(v, A,0,N-1);
+}
 
 int main() {
     const int ARRAY_SIZE = 10000;
@@ -19,6 +29,7 @@ int main() {
     for (int i = 0; i < ARRAY_SIZE; ++i) {
         myNumbers[i] = i;
     }
+    /* Provided code search:
     for (int findN = 1000; findN < 10000; findN += 1000) {
         high_resolution_clock::time_point startTime = high_resolution_clock::now();
         int i = 0;
@@ -30,10 +41,71 @@ int main() {
                 cout << "Found " << findN << " at position " << i << "\n";
                 break;
             }
-        }
+        }       
+        high_resolution_clock::time_point stopTime = high_resolution_clock::now();
+        duration<double, std::milli> timeTaken = (stopTime - startTime);
+        cout << "That search zipped by in " << timeTaken.count() << " milliseconds.\n";
+        
+    }
+    */
+    /* Provided code search timing:
+
+        Found 1000 at position 1000
+        That search zipped by in 2.5543 milliseconds.
+        Found 2000 at position 2000
+        That search zipped by in 0.9138 milliseconds.
+        Found 3000 at position 3000
+        That search zipped by in 1.7079 milliseconds.
+        Found 4000 at position 4000
+        That search zipped by in 1.4002 milliseconds.
+        Found 5000 at position 5000
+        That search zipped by in 0.7587 milliseconds.
+        Found 6000 at position 6000
+        That search zipped by in 0.4111 milliseconds.
+        Found 7000 at position 7000
+        That search zipped by in 1.1393 milliseconds.
+        Found 8000 at position 8000
+        That search zipped by in 0.7904 milliseconds.
+        Found 9000 at position 9000
+        That search zipped by in 1.1263 milliseconds.
+    */
+    /* Binary Search*/
+    for (int findN = 1000; findN < 10000; findN += 1000) {
+        high_resolution_clock::time_point startTime = high_resolution_clock::now();
+        int i = 0;
+        while (i < ARRAY_SIZE) {
+            if (bSrch(i, myNumbers, ARRAY_SIZE) != findN) {
+                i++;
+            }
+            else {
+                cout << "Found " << findN << " at position " << i << "\n";
+                break;
+            }
+        }       
         high_resolution_clock::time_point stopTime = high_resolution_clock::now();
         duration<double, std::milli> timeTaken = (stopTime - startTime);
         cout << "That search zipped by in " << timeTaken.count() << " milliseconds.\n";
     }
+    /* Binary Search timing:
+    Found 1000 at position 1000
+    That search zipped by in 1.0905 milliseconds.
+    Found 2000 at position 2000
+    That search zipped by in 1.0635 milliseconds.
+    Found 3000 at position 3000
+    That search zipped by in 1.2658 milliseconds.
+    Found 4000 at position 4000
+    That search zipped by in 1.4534 milliseconds.
+    Found 5000 at position 5000
+    That search zipped by in 1.0055 milliseconds.
+    Found 6000 at position 6000
+    That search zipped by in 1.0415 milliseconds.
+    Found 7000 at position 7000
+    That search zipped by in 1.3224 milliseconds.
+    Found 8000 at position 8000
+    That search zipped by in 1.4714 milliseconds.
+    Found 9000 at position 9000
+    That search zipped by in 2.6885 milliseconds.
+    */
+
     return 0;
 }
